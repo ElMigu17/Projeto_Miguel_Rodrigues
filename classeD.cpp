25a3439752d3a93de41b70d9944897bfd3f96001
Miguel <rgomiguel17@gmail.com>
2020-11-22 22:32:37 -0300

    adicionada a classe D

---
+++ classeD.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+class D{
+    private:
+        int D1;
+        float D2;
+    
+    public:
+        D(){
+            D1 = 0;
+            D2 = 0;    
+        }
+
+        int getD1(){
+            return D1;
+        }   
+        void setD1(int D1){
+            this.D1 = D1;
+        }  
+        float getD2(){
+            return D2;
+        }
+        void setD2(int D2){
+            this.D2 = D2;
+        }  
+
+        void MD1(){
+            cout<<"MD1"<<endl;
+        }
+        void MD2(){
+            cout<<"MD2"<<endl;
+        }
+}
+
+int main(){
+}
